0cefd3a78da7bc517b9ad6c41f1ebfdbd251608c
abhinandhks <abhinandhks57@gmail.com>
2025-08-09 19:43:45 +0530

    initial updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"
 import Hero from "./pages/Hero";
@@ -20,6 +21,7 @@ function App() {
         <Route path="/about" element={<About/>}/>
       </Routes>
       <Footer/>
+      <Analytics />
     </>
   );
 }
